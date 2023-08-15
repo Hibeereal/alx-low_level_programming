a79d451f3d563da15ddf0da839797857182e7728
Hibeekay <akinolaibukun182@gmail.com>
2023-08-15 14:12:32 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _islower - "Write a function that checks for lowercase character."
+ * @c: the character to be checked
+ *
+ * Retun: 1 else return 0
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}

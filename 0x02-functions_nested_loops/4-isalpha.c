367e503966aa863997af1635b0453746becb56a5
Hibeekay <akinolaibukun182@gmail.com>
2023-08-15 14:28:59 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - "Write a function that checks for alphabetic character."
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase or upercase and 0 if otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c >= 'z') || (c <= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

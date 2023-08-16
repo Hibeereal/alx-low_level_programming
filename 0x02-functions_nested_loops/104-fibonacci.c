df1a4bc7ac3fb0beb493aee96609b582ec4b08d1
Hibeekay <akinolaibukun182@gmail.com>
2023-08-16 03:15:07 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
+
+	while (i < 98)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu\n", next);
+
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

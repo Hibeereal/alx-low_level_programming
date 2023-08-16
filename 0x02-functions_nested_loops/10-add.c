fa07194bb0e952215a41f1cc9c84ac522c2647fa
Hibeekay <akinolaibukun182@gmail.com>
2023-08-16 00:26:43 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - "Write a function that adds two integers and returns the
+ * result"
+ * @a: first integer value
+ * @c: second in value
+ *
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int c)
+{
+	int result;
+
+	result = a + c;
+
+	return (result);
+}

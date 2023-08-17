52f87a0516c73eace5dc3183f71b696f7ad35cd4
Hibeekay <akinolaibukun182@gmail.com>
2023-08-17 14:33:15 -0700

    uppercse check

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - "Write a function that multiplies two integers."
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	_putchar(c + '0');
+	return (c);
+}

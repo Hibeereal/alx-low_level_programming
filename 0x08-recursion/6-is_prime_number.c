46fbc2d0fcd9f77922d628f72a2293446ffb9617
Hibeekay <akinolaibukun182@gmail.com>
2023-08-31 13:51:45 -0700

    prime num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,8 +15,8 @@ int is_prime_number(int n)
 	if (n <= 1)
 	{
 		return (0);
-		return (actual_prime(n, n - 1));
 	}
+	return (actual_prime(n, n - 1));
 	return (0);
 }
 
@@ -35,10 +35,10 @@ int actual_prime(int n, int i)
 	{
 		return (1);
 	}
-	if (n % i == 0 && i > 0)
+	if (n % i == 0)
 	{
 		return (0);
-		return (actual_prime(n, i - 1));
 	}
+	return (actual_prime(n, i - 1));
 	return (0);
 }

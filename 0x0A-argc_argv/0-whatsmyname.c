2a64ad655a3a7602e93eb57894e1ee6d22d2a09f
Hibeekay <akinolaibukun182@gmail.com>
2023-09-01 13:11:27 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ * @argc: command line comman
+ * @argv: array argument
+ *
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int main(int argc, char *argv[])
+{
+
+	char *name;
+
+	if (argc == 1)
+	{
+
+	name = argv[0];
+
+	while (*name)
+	{
+		_putchar(*name);
+		name++;
+	}
+	_putchar('\n');
+	}
+	return (0);
+}

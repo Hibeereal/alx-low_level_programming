bf74eb21682900cd0185bf3a7ee3de61799752eb
Hibeekay <akinolaibukun182@gmail.com>
2023-09-11 15:18:18 -0700

    print do struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nill)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
 }

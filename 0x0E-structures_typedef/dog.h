2fec266945e45e287a3396fdff26725bbd8c87ee
Hibeekay <akinolaibukun182@gmail.com>
2023-09-11 11:49:47 -0700

    define struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Define a new type struct dog with the following elements
@@ -13,3 +14,4 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif

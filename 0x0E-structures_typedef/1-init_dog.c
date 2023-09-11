64cbe1226e2ea861b0e3b5ba4ea553a5e03b25b1
Hibeekay <akinolaibukun182@gmail.com>
2023-09-11 14:26:43 -0700

    struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,6 +20,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = (char *)malloc(strlen(owner) + 1);
 
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
+
 	if (d->name == NULL || d->owner == NULL)
 	{
 		exit(1);
